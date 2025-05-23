bd6f3fb8ecb2e30c934df02e6ae4b59beb29dfc5
Víctor Velasco-Pardo <vvelasco@ed.ac.uk>
2025-05-23 17:21:53 +0100

    Separate dataset with dynamic and static covariates; Use general notation (rather than biostats); landmarks given at the time of prediction (#2)
    This PR refactors landmarking functionality to separate static and dynamic covariates, standardizes the notation, and updates landmark time handling. Key changes include commenting out the legacy survival methods for LOCF, LME4, and Baseline approaches, updating the Landmarking S4 class with new slots (data_static and data_dynamic) while removing horizons and baseline_covariates, and revamping the longitudinal and survival fitting functions.

--- R/landmarking.R
+++ R/landmarking.R
@@ -1,10 +1,9 @@
 #' An S4 class for a landmarking analysis
 #'
 #' @slot landmarks Array of landmark times.
-#' @slot horizons Array of horizon times. Must have the same length as horizons.
-#' @slot dataset Dataframe containing relevant data in long format.
+#' @slot data_static Data frame with id, static covariates, censoring indicator and event time.
+#' @slot data_dynamic Data frame in long format with id, measurement, measurement time and covariate measured.
 #' @slot event_indicator Name of the column indicating event or censoring.
-#' @slot baseline_covariates Names of the columns for baseline covariates.
 #' @slot biomarkers List whose name indicate columns for biomarker measurements, and values indicate columns for biomarker measurement times
 #' @slot ids Name of the column indicating patient id.
 #' @slot event_time Name of the column indicating time of the event/censoring.
@@ -17,10 +16,9 @@
 setClass("Landmarking",
   slots = c(
     landmarks = "numeric",
-    horizons = "numeric",
-    dataset = "data.frame",
+    data_static = "data.frame",
+    data_dynamic = "data.frame",
     event_indicator = "character",
-    baseline_covariates = "character",
     biomarkers = "list",
     ids = "character",
     event_time = "character",
@@ -33,23 +31,20 @@ setClass("Landmarking",
 
 #' Creates an S4 class for a landmarking analysis
 #'
-#' @slot landmarks Array of landmark times.
-#' @slot horizons Array of horizon times. Must have the same length as horizons.
-#' @slot dataset Dataframe containing relevant data in long format.
+#' @slot data_static Data frame with id, static covariates, censoring indicator and event time.
+#' @slot data_dynamic Data frame in long format with id, measurement, measurement time and covariate measured.
 #' @slot event_indicator Name of the column indicating event or censoring.
-#' @slot baseline_covariates Names of the columns for baseline covariates.
+#' @slot biomarkers List whose name indicate columns for biomarker measurements, and values indicate columns for biomarker measurement times
 #' @slot ids Name of the column indicating patient id.
 #' @slot event_time Name of the column indicating time of the event/censoring.
 #'
 #' @returns An object of class Landmarking
 #' @export
-Landmarking <- function(landmarks, horizons, dataset, event_indicator, baseline_covariates, biomarkers, ids, event_time) {
+Landmarking <- function(data_static, data_dynamic, event_indicator, biomarkers, ids, event_time) {
   new("Landmarking",
-      landmarks = landmarks,
-      horizons = horizons,
-      dataset = dataset,
+     data_static = data_static,
+      data_dynamic = data_dynamic,
       event_indicator = event_indicator,
-      baseline_covariates = baseline_covariates,
       biomarkers = biomarkers,
       ids = ids,
       event_time = event_time,
@@ -60,28 +55,15 @@ Landmarking <- function(landmarks, horizons, dataset, event_indicator, baseline_
   )
 }
 
-setValidity("Landmarking", function(object) {
-  if (length(object@landmarks) != length(object@horizons)) {
-    "@landmarks and @horizons must have the same length"
-  } else {
-    TRUE
-  }
-})
-
-setGeneric("fit_longitudinal", function(x) standardGeneric("fit_longitudinal"))
-setGeneric("fit_survival", function(x) standardGeneric("fit_survival"))
-
+# show method for class "Landmarking"
 setMethod(
   f = "show",
   signature = "Landmarking",
   definition = function(object) {
     cat("Summary of Landmarking Object:\n")
     cat("  Landmarks:", object@landmarks, "\n")
-    cat("  Horizons:", object@horizons, "\n")
-    cat("  Number of observations:", nrow(object@dataset), "\n")
+    cat("  Number of observations:", nrow(object@data_static), "\n")
     cat("  Event indicator:", object@event_indicator, "\n")
-    cat("  Baseline covariates:", paste(object@baseline_covariates, collapse = ", "), "\n")
-    cat("  Number of unique IDs:", length(unique(object@dataset[, object@ids])), "\n")
     cat("  Event time:", object@event_time, "\n")
     cat("  Risk sets:", "\n")
     if (length(object@risk_sets) > 0) {
@@ -96,21 +78,9 @@ setMethod(
 setGeneric("getLandmarks", function(object) standardGeneric("getLandmarks"))
 setMethod("getLandmarks", "Landmarking", function(object) object@landmarks)
 
-# Accessor for horizons
-setGeneric("getHorizons", function(object) standardGeneric("getHorizons"))
-setMethod("getHorizons", "Landmarking", function(object) object@horizons)
-
-# Accessor for dataset
-setGeneric("getDataset", function(object) standardGeneric("getDataset"))
-setMethod("getDataset", "Landmarking", function(object) object@dataset)
-
 # Accessor for event
 setGeneric("getEvent", function(object) standardGeneric("getEvent"))
 setMethod("getEvent", "Landmarking", function(object) object@event)
-
-# Accessor for baseline_covariates
-setGeneric("getBaselineCovariates", function(object) standardGeneric("getBaselineCovariates"))
-setMethod("getBaselineCovariates", "Landmarking", function(object) object@baseline_covariates)
 
 # Accessor for ids
 setGeneric("getIds", function(object) standardGeneric("getIds"))
@@ -124,40 +94,146 @@ setMethod("getEventTime", "Landmarking", function(object) object@event_time)
 setGeneric("getRiskSets", function(object) standardGeneric("getRiskSets"))
 setMethod("getRiskSets", "Landmarking", function(object) object@risk_sets)
 
-setGeneric("compute_risk_sets", function(x) standardGeneric("compute_risk_sets"))
-setMethod("compute_risk_sets", "Landmarking", function(x) {
-  x@risk_sets <- list()
-  for(i in 1:length(x@landmarks)) {
-    x@risk_sets[[i]] <- which(x@dataset[, x@event_time] >= x@landmarks[i])
+#' Compute the list of individuals at risk at given landmark times,
+#' and stores them in an object of class Landmarking
+#'
+#' @param x An object of type Landmarking
+#' @param landmarks Vector of landmark times
+#' @param ...
+#'
+#' @returns An
+#' @export
+#'
+#' @examples
+setGeneric("compute_risk_sets", function(x, landmarks, ...) standardGeneric("compute_risk_sets"))
+setMethod("compute_risk_sets", "Landmarking", function(x, landmarks, ...) {
+  if (length(landmarks) == 1) { # If the vector of landmark times is of length 1
+    if (landmarks %in% x@landmarks) { # Risk set for given landmark time was already in memory
+      warning("Risk set for landmark time ", landmarks, " already computed")
+    }
+    x@landmarks <- c(x@landmarks, landmarks) # Add landmark time to the Landmarking object
+    # Compute risk set for given landmark time
+    x@risk_sets[[as.character(landmarks)]] <- which(x@data_static[, x@event_time] >= landmarks)
+  } else {
+    # Recursion to compute risk sets one-by-one
+    x <- compute_risk_sets(x, landmarks[1])
+    x <- compute_risk_sets(x, landmarks[-1])
   }
   x
 })
 
+# Accessor for survival fits
 setGeneric("getSurvivalFits", function(object) standardGeneric("getSurvivalFits"))
 setMethod("getSurvivalFits", "Landmarking", function(object) object@survival_fits)
 
-setGeneric("longitudinal_wrapper", function(x, ...) standardGeneric("longitudinal_wrapper"), signature = "x")
-
-setGeneric("fit_longitudinal", function(x) standardGeneric("fit_longitudinal"))
-setMethod("fit_longitudinal", "Landmarking", function(x) {
-  for (i in 1:length(x@landmarks)) {
-    x@longitudinal_fits[[i]] <- list()
-    x@longitudinal_predictions[[i]] <- list()
-    for (biomarker in names(x@biomarkers)) {
-      time_covariate <- x@biomarkers[[biomarker]]
-      x@longitudinal_fits[[i]][[biomarker]] <- longitudinal_wrapper(x, i, biomarker)
-      x@longitudinal_predictions[[i]][[biomarker]] <- predict(x@longitudinal_fits[[i]][[biomarker]])
+#' Title
+#'
+#' @param x
+#' @param landmarks
+#' @param horizons
+#' @param method
+#' @param dynamic_covariates
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+setGeneric("fit_survival", function(x, landmarks, horizons, method, dynamic_covariates = c()) standardGeneric("fit_survival"))
+setMethod("fit_survival", "Landmarking", function(x, landmarks, horizons, method, dynamic_covariates = c()) {
+  # Check that method for survival analysis is available
+  if (!(method %in% c("survfit", "coxph"))) {
+    message("Method ", method, " not supported")
+  }
+  # Check that vectors of landmark times and horizons have the same length
+  if (length(landmarks) != length(horizons)) {
+    message("Arguments landmarks and horizons must have equal length")
+  }
+  if (length(landmarks) == 1) { # Base case for recursion
+    if (!(landmarks %in% x@landmarks)) {
+      message("Risk set for landmark time ", landmarks, " has not been computed")
     }
+    # Construct formula for survival analysis
+    survival_formula <- paste0("Surv(", x@event_time, ", ", x@event_indicator, ") ~ ")
+    # Recover risk sets (ids of individuals who are at risk at landmark time)
+    at_risk_individuals <- x@risk_sets[[as.character(landmarks)]]
+    # Construct dataset for survival analysis (censor observations of events past horizon time)
+    dataset <- data_static[at_risk_individuals, ] |>
+      mutate(event_status = ifelse(event_time > horizons, 0, event_status), event_time = ifelse(event_time > horizons, horizons, event_time))
+    # Add time-varying covariates to formula and dataset for survival analysis
+    if (length(dynamic_covariates) == 0) {
+      survival_formula <- as.formula(paste0(survival_formula, "1"))
+    } else {
+      survival_formula <- as.formula(paste0(survival_formula, paste(dynamic_covariates, collapse = " + ")))
+      dataset <- cbind(dataset, do.call(cbind, x@longitudinal_predictions[[as.character(landmarks)]])[at_risk_individuals, ])
+    }
+    # Call to method that performs survival analysis
+    if (method == "coxph") {
+     x@survival_fits[[as.character(landmarks)]] <- survival::coxph(survival_formula, data = dataset)
+    } else if (method == "survfit") {
+      x@survival_fits[[as.character(landmarks)]] <- survival::coxph(survival_formula, data = dataset)
+    }
+  } else {
+    # Recursion
+    x <- fit_survival(x, landmarks[1], horizons[1], method, dynamic_covariates)
+    x <- fit_survival(x, landmarks[-1], horizons[-1], method, dynamic_covariates)
   }
   x
 })
 
-setGeneric("survival_wrapper", function(x, ...) standardGeneric("survival_wrapper"), signature = "x")
-
-setGeneric("fit_survival", function(x) standardGeneric("fit_survival"))
-setMethod("fit_survival", "Landmarking", function(x) {
-  for (i in 1:length(x@landmarks)) {
-    x@survival_fits[[i]] <- survival_wrapper(x, i)
+#' Title
+#'
+#' @param x
+#' @param landmarks
+#' @param method
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+setGeneric("fit_longitudinal", function(x, landmarks, method, static_covariates) standardGeneric("fit_longitudinal"))
+setMethod("fit_longitudinal", "Landmarking", function(x, landmarks, method, static_covariates) {
+  # Check that the method for longitudinal analysis is implemented
+  if (!(method %in% c("lme4"))) {
+    message("Method ", method, " not supported")
+  }
+  # Base case for recursion
+  if (length(landmarks) == 1) {
+    # Check that relevant risk set is available
+    if (!(landmarks %in% x@landmarks)) {
+      message("Risk set for landmark time ", landmarks, " has not been computed")
+    }
+    # Create list for storing model fits, and predictions, for longitudinal analysis
+    x@longitudinal_fits[[as.character(landmarks)]] <- list()
+    x@longitudinal_predictions[[as.character(landmarks)]] <- list()
+    # Loop that iterates over all time-varying covariates, to fit a longitudinal model for the underlying trajectories
+    for (predictor in names(landmarking_object@biomarkers)) {
+      # Risk set for the landmark time
+      at_risk_individuals <- x@risk_sets[[as.character(landmarks)]]
+      # Construct dataset for the longitudinal analysis (static measurements + time-varying covariate and its recording time)
+      dataframe <- x@data_dynamic |>
+        filter(covariate == predictor) |>
+        filter(id %in% at_risk_individuals) |>
+        filter(measurement_time <= landmarks) |>
+        left_join(data_static, by = join_by(id))
+      # Construct formula for longitudinal analysis
+      longitudinal_formula <- as.formula(paste0("measurement ~ ", paste(static_covariates, collapse = " + "), " + (measurement_time|id)"))
+      # Fit longitudinal model according to chosen method
+      if (method == "lme4") {
+        # Model fit
+        lme4_fit <- lme4::lmer(longitudinal_formula, data = dataframe)
+        x@longitudinal_fits[[as.character(landmarks)]][[predictor]] <- lme4_fit
+        # Model predictions at landmark tiem
+        x@longitudinal_predictions[[as.character(landmarks)]][[predictor]] <-
+          predict(lme4_fit,
+                  newdata =x@data_static |> select(id, matches(paste0(static_covariates, collapse="|"))) |> mutate(measurement_time = landmarks),
+                  allow.new.levels = TRUE
+          )
+      }
+    }
+  } else {
+    # Recursion
+    x <- fit_longitudinal(x, landmarks[1], method, static_covariates)
+    x <- fit_longitudinal(x, landmarks[-1], method, static_covariates)
   }
   x
 })
--- R/landmarking_lme4_survival.R
+++ R/landmarking_lme4_survival.R
@@ -1,45 +1,45 @@
-setClass("LandmarkingLme4Survival",
-  contains = "Landmarking"
-)
-
-LandmarkingLme4Survival <- function(landmarks, horizons, dataset, event_indicator, baseline_covariates, biomarkers, ids, event_time) {
-  new("LandmarkingLme4Survival",
-      landmarks = landmarks,
-      horizons = horizons,
-      dataset = dataset,
-      event_indicator = event_indicator,
-      baseline_covariates = baseline_covariates,
-      biomarkers = biomarkers,
-      ids = ids,
-      event_time = event_time,
-      risk_sets = list(),
-      longitudinal_fits = list(),
-      longitudinal_predictions = list(),
-      survival_fits = list()
-  )
-}
-
-setMethod("longitudinal_wrapper", "LandmarkingLme4Survival", function(x, landmark_index, biomarker) {
-  longitudinal_formula <- as.formula(paste0(
-    biomarker, " ~ ",
-    paste(x@baseline_covariates, collapse =  " + "), " + ",
-    "(", x@biomarkers[[biomarker]], "|" , x@ids, ")"
-  ))
-  at_risk_individuals <- x@risk_sets[[landmark_index]]
-  lme4::lmer(longitudinal_formula, data = x@dataset[at_risk_individuals, ])
-})
-
-setMethod("survival_wrapper", "LandmarkingLme4Survival", function(x, landmark_index) {
-  at_risk_individuals <- x@risk_sets[[landmark_index]]
-  dataset <- x@dataset[at_risk_individuals, ] |> group_by(!! sym(x@ids)) |> slice_tail(n=1) |> ungroup()
-  for (i in 1:length(x@biomarkers)) {
-    marker <- names(x@biomarkers)[i]
-    dataset <- cbind(dataset, x@longitudinal_predictions[[landmark_index]][[marker]])
-    colnames(dataset)[ncol(dataset)] <- marker
-  }
-  survival_formula <- as.formula(paste0(
-    "Surv(", x@event_time, ", ", x@event_indicator, ") ~ ", paste0(names(x@biomarkers), collapse = " + ")
-  ))
-
-  survival::survfit(survival_formula, data = dataset)
-})
+### setClass("LandmarkingLme4Survival",
+###   contains = "Landmarking"
+### )
+###
+### LandmarkingLme4Survival <- function(landmarks, horizons, dataset, event_indicator, baseline_covariates, biomarkers, ids, event_time) {
+###   new("LandmarkingLme4Survival",
+###       landmarks = landmarks,
+###       horizons = horizons,
+###       dataset = dataset,
+###       event_indicator = event_indicator,
+###       baseline_covariates = baseline_covariates,
+###       biomarkers = biomarkers,
+###       ids = ids,
+###       event_time = event_time,
+###       risk_sets = list(),
+###       longitudinal_fits = list(),
+###       longitudinal_predictions = list(),
+###       survival_fits = list()
+###   )
+### }
+###
+### setMethod("longitudinal_wrapper", "LandmarkingLme4Survival", function(x, landmark_index, biomarker) {
+###   longitudinal_formula <- as.formula(paste0(
+###     biomarker, " ~ ",
+###     paste(x@baseline_covariates, collapse =  " + "), " + ",
+###     "(", x@biomarkers[[biomarker]], "|" , x@ids, ")"
+###   ))
+###   at_risk_individuals <- x@risk_sets[[landmark_index]]
+###   lme4::lmer(longitudinal_formula, data = x@dataset[at_risk_individuals, ])
+### })
+###
+### setMethod("survival_wrapper", "LandmarkingLme4Survival", function(x, landmark_index) {
+###   at_risk_individuals <- x@risk_sets[[landmark_index]]
+###   dataset <- x@dataset[at_risk_individuals, ] |> group_by(!! sym(x@ids)) |> slice_tail(n=1) |> ungroup()
+###   for (i in 1:length(x@biomarkers)) {
+###     marker <- names(x@biomarkers)[i]
+###     dataset <- cbind(dataset, x@longitudinal_predictions[[landmark_index]][[marker]])
+###     colnames(dataset)[ncol(dataset)] <- marker
+###   }
+###   survival_formula <- as.formula(paste0(
+###     "Surv(", x@event_time, ", ", x@event_indicator, ") ~ ", paste0(names(x@biomarkers), collapse = " + ")
+###   ))
+###
+###   survival::survfit(survival_formula, data = dataset)
+### })
