dc0e77bfe9906c906ded807f42b59d37a9d63331
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2025-05-26 15:21:46 +0100

    Add dataset (epileptic)

---
+++ data-raw/epileptic.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+epileptic <-  joineR::epileptic
+epileptic <- epileptic %>%
+  select(id, time, with.time, with.status, dose, treat, age, gender, learn.dis)
+
+usethis::use_data(epileptic, overwrite = TRUE)
