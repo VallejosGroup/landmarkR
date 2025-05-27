76c2f8f51b8409bdf6c57e31e75eac8eea820966
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2025-05-27 14:14:15 +0100

    Use landmarkR everywhere

--- R/landmarkR-package.R
+++ R/landmarkR-package.R
@@ -1,10 +1,10 @@
-#' landmarkeR
+#' landmarkR
 ## usethis namespace: start
 #' @importFrom methods new
 #' @importFrom stats as.formula predict
 #' @importFrom utils head
 ## usethis namespace: end
 #' @docType package
-#' @name landmarkeR
+#' @name landmarkR
 #' @keywords internal
 "_PACKAGE"
